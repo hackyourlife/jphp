9df84992f16c477f271155ef31715b4a8ac29555
hackyourlife <root@hackyourlife>
2015-03-02 18:43:41 +0100

    github link

--- src/org/hackyourlife/webpage/Layout.java
+++ src/org/hackyourlife/webpage/Layout.java
@@ -6,6 +6,6 @@ public class Layout extends Webpage {
 		setStylesheet("css/style.css");
 		setFooter("<div id=\"footer\"><a href=\"https://www.lima-city.de/homepage/ref:250835\">lima-city.de</a>"
 			+ " • <a href=\"http://hackyourlife.lima-city.de\">hackyourlife</a>"
-			+ " • <a href=\"https://github.com/hackyourlife/\">GitHub</a></div>");
+			+ " • <a href=\"https://github.com/hackyourlife/jphp\">GitHub</a></div>");
 	}
 }
